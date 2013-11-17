3bc3969e9f35a401d09bfad545090c76abf7be2d
Aswini S <ashumeow@live.com>
2013-11-17 23:04:35 +0530

    Update classesobjects.cpp

--- classesobjects.cpp
+++ classesobjects.cpp
@@ -1,26 +1,26 @@
-#include<iostream.h>
-static float PI = 3.14;
+#include<iostream>
+static float PI=3.14;
 class Cylinder
 {
 private:
 float radius;
 float height;
 public:
-Cylinder(float, float)
+Cylinder(float,float)
 {
 float volume(void);
 };
-Cylinder :: Cylinder(float a, float b)
+Cylinder::Cylinder(float a,float b)
 {
 radius=a;
 height=b;
 }
-float Cylinder :: volume(void)
+float Cylinder::volume(void)
 {
-return PI * radius * radius * height;
+return PI*radius*radius*height;
 }
 void main()
 {
-Cylinder cyl = Cylinder(10,8);
+Cylinder cyl=Cylinder(10,8);
 cout<<"Volume of the cylinder is"<<cyl.volume();
 }
