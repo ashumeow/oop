63babb7cd7ac5db44cc69cc5918fc50e0922a2a2
Aswini S <ashumeow@live.com>
2013-11-17 22:17:14 +0530

    Update classes.java

--- classes.java
+++ classes.java
@@ -5,25 +5,20 @@ class classes
 {
 public static void main (String[] args) throws java.lang.Exception
 {
-static final double PI = 3.14;
-static int count = 0;
+double PI = 3.14;
 double radius;
 double height;
-static int.getCount()
-{
-return count;
-}
 double volume()
 { 
 return PI * radius * radius * height;
 }
 public static void main (String args[])
 {
-Cylinder c1;
-c1=new Cylinder(10,5);
+classes c1;
+c1=new classes(10,5);
 System.out.println("Volume of cylinder is" +c1.volume());
-Cylinder c2;
-c2=new Cylinder(15,5);
+classes c2;
+c2=new classes(15,5);
 System.out.println("Volume of cylinder is" +c2.volume());
 }
 }
