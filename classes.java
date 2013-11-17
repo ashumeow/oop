b8e8315837cf550f91b0a5c200cc5da9bbd6c763
Aswini S <ashumeow@live.com>
2013-11-17 13:53:19 +0530

    Create classes.java

---
+++ classes.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class classes
+{
+public static void main (String[] args) throws java.lang.Exception
+{
+static final double PI = 3.14;
+static int count = 0;
+double radius;
+double height;
+static int.getCount()
+{
+return count;
+}
+double volume()
+{ 
+return PI * radius * radius * height;
+}
+public static void main (String args[])
+{
+Cylinder c1;
+c1=new Cylinder(10,5);
+System.out.println("Volume of cylinder is" +c1.volume());
+Cylinder c2;
+c2=new Cylinder(15,5);
+System.out.println("Volume of cylinder is" +c2.volume());
+}
+}
+}
