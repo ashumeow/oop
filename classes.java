217bf0caf1febda960accc797c65af1236d057f3
Aswini S <ashumeow@live.com>
2014-11-16 00:00:49 +0530

    Update classes.java

--- classes.java
+++ classes.java
@@ -1,18 +1,19 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class classes
+public class classes
 {
-public static void main (String[] args) throws java.lang.Exception
+public static void main (String[] args) throws IOException
 {
 double PI = 3.14;
 double radius;
 double height;
-double volume()
+}
+public double int volume()
 { 
 return PI * radius * radius * height;
 }
-void main ()
+void main()
 {
 classes c1;
 c1=new classes(10,5);
@@ -20,6 +21,5 @@ System.out.println("Volume of cylinder is" +c1.volume());
 classes c2;
 c2=new classes(15,5);
 System.out.println("Volume of cylinder is" +c2.volume());
-}
 }
 }
